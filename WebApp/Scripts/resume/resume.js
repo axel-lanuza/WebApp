cb4862e9a8699f5d0a442361dc5e0f5845ca37ca
zhanqun <zhanqun@ibs-tech.com.cn>
2015-03-12 15:48:19 +0800

    add resume and template files

---
+++ WebApp/Scripts/resume/resume.js
@@ -0,0 +1,17 @@
+﻿var template;
+$(function () {
+    var height = this.documentElement.clientHeight;
+    var width = this.documentElement.clientWidth;
+    var container = $('#container');
+    $('body').load('View/resume/template.html', function (responseTxt, statusTxt, xhr) {
+        $(responseTxt).appendTo(container);
+        template = $('.template');
+        template.css('height', height);
+        template.css('width', 800);
+        load();
+    });
+});
+
+function load() {
+
+}
