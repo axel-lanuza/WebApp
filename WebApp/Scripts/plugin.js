6a6d7f5375cb539213755b3b4960f88dc281b4fa
zhanqun <zhanqun@ibs-tech.com.cn>
2015-03-12 17:03:14 +0800

    add back button to return home.html page

---
+++ WebApp/Scripts/plugin.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    var height = this.documentElement.clientHeight;
+    var width = this.documentElement.clientWidth;
+    var body = $('body');
+    $('body').append($('<div class="back"></div>'));
+    var back = $('.back');
+    back.click(function () {
+        location = 'home.html';
+    });
+    back.css('left', '10');
+    back.css('top', height - 80);
+});
--- WebApp/Scripts/resume/resume.js
+++ WebApp/Scripts/resume/resume.js
@@ -2,10 +2,10 @@
 $(function () {
     var height = this.documentElement.clientHeight;
     var width = this.documentElement.clientWidth;
+    var doc = this.documentElement;
     var container = $('#container');
-    $('body').load('View/resume/template.html', function (responseTxt, statusTxt, xhr) {
-        $(responseTxt).appendTo(container);
-        template = $('.template');
+    $('#container').load('View/resume/template.html', function (responseTxt, statusTxt, xhr) {
+        template = $('.template', this);
         template.css('height', height);
         template.css('width', 800);
         load();
