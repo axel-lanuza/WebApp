2106e5d7d0fec8f1652802458ed5716d14af777a
zhanqun <zhanqun@ibs-tech.com.cn>
2015-03-13 08:25:44 +0800

    add detail resume info

--- WebApp/Scripts/resume/resume.js
+++ WebApp/Scripts/resume/resume.js
@@ -7,6 +7,10 @@ $(function () {
     $('#container').load('View/resume/template.html', function (responseTxt, statusTxt, xhr) {
         template = $('.template', this);
         template.css('height', height);
+        if (width < 800)
+            template.css('margin-left', '0');
+        else if (width < 1000)
+            template.css('margin-left', '10%');
         template.css('width', 800);
         load();
     });
