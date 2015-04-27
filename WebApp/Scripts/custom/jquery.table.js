d54c31c7e9de8c6e831402ec75f8e05281ad489b
zhanqun <zhanqun@ibs-tech.com.cn>
2015-04-27 16:58:48 +0800

    update the plugin jquery.table.js

--- WebApp/Scripts/custom/jquery.table.js
+++ WebApp/Scripts/custom/jquery.table.js
@@ -1,36 +1,166 @@
 ﻿(function ($) {
-    function _bindingColumns(target) {
-        var grid = $(target);
-        var state = grid.data('table');
-        var option = state.options;
-        if (!option.headerDiv) {
-            var css = _generalStyle();
-            grid.append(css);
-
-            if (option.frozenable) {
-                option.headerDiv = $('<div><div class="_frozened _header" style="overflow:hidden;"></div><div class="_unfrozened _header" style="overflow:hidden;"></div></div>').appendTo(grid);
-                option.rowDiv = $('<div><div class="_frozened _content" style="overflow:auto;"></div><div class="_unfrozened _content" style="overflow:auto;"></div></div>').appendTo(grid);
-            } else {
-                option.headerDiv = $('<div><div class="_header" style="overflow:hidden"></div></div>').appendTo(grid);
-                option.rowDiv = $('<div><div class="_content" style="overflow:auto"></div></div>').appendTo(grid);
-            }
-            $.each($('._header', option.headerDiv), function (i, node) {
-                $('<table cellspacing="0" cellpadding="0" border="0"></table>').appendTo(node);
-            });
-            $.each($('._content', option.rowDiv), function (i, node) {
-                $('<table cellspacing="0" cellpadding="0" border="0"></table>').appendTo(node);
-            });
-        }
-    }
-
-    function _generalStyle() {
+    function generalStyle(options) {
         var css = '<style type="text/css">';
-        css += '._header{height:24px;background-color:lightyellow;overflow:hidden;}';
-        css += '._content{height:200px;background-color:lightblue;overflow:hidden;}';
-        css += '._frozened{float:left;width:100px;background-color:green;}';
-        css += '._unfrozened{float:left;width:100px;background-color:blue;}';
+        if (options.border)
+            css += 'table,td,th{border-style:solid;border-width:1px;border-collapse:collapse;}';
+        else
+            css += 'table,td,th{padding:0;border-collapse:collapse;}';
+        css += '#' + options.id + '{padding:0;margin:0;width:' + (options.width === 'auto' ? '100%' : (options.width + 'px')) + ';height:' + (options.height === 'auto' ? '100%' : (options.height + 'px')) + ';}';
+        css += '.c-panel{overflow:auto;width:100%;height:100%;}';
+        css += '.c-head{background-color:#93c9f2;}';
+        css += '.c-rows{}';
+        css += '.c-rows >tr:hover{background-color:#f2e6e3}';
+        css += '.c-foot{height:16px;width:100%;margin:0;padding:0px;background-color:#b9c7e1;font-size:12px;}';
+        css += '.c-prev,.c-next,.c-first,.c-last{margin:5px;cursor:pointer;}';
         css += '</style>';
         return css;
+    }
+
+    function bindingClass(target, options) {
+        var id = 'table_' + (new Date()).getTime();
+        $.extend(options, { id: id });
+        var sl = '<div id="' + id + '" oncontextmenu="return false">';
+        sl += '<div class="c-panel">';
+        sl += '<table><thead><tr class="c-head"></tr></thead><tbody class="c-rows"></tbody></table>';
+        sl += '</div>';
+        sl += '<div class="c-foot"><span class="c-first">首页</span><span class="c-prev">上一页</span><span class="c-next">下一页</span><span class="c-last">尾页</span><span class="c-info"></span></div></div>';
+        $(sl).appendTo(target);
+        var css = generalStyle(options);
+        target.append(css);
+        var container = $('#' + options.id, target);
+        options.rowsDiv = $('.c-rows', container);
+        options.first = $('.c-foot .c-first', target).click(function () {
+            options.pageNumber = 0;
+            bindingRows(options);
+        });
+        options.last = $('.c-foot .c-last', target).click(function () {
+            options.pageNumber = options.totalCount - 1;
+            bindingRows(options);
+        });
+        options.prev = $('.c-foot .c-prev', target).click(function () {
+            bindingRows(options, true, false);
+        });
+        options.next = $('.c-foot .c-next', target).click(function () {
+            bindingRows(options, false, true);
+        });
+        options.info = $('.c-foot .c-info', target);
+        if (options.onContextMenu !== undefined) {
+            container.bind("contextmenu", function (e) {
+                options.onContextMenu(e);
+            });
+        }
+        options.rowsDiv.scroll(function () {
+            options.scrollLeft = options.rowsDiv.scrollLeft();
+        });
+    }
+
+    function clear(target) {
+        var table = $(target);
+        var options = table.data('table').options;
+        if (options.headerDiv)
+            options.headerDiv.empty();
+        if (options.rowsDiv)
+            options.rowsDiv.empty();
+    }
+
+    function bindingData(target) {
+        var table = $(target);
+        var options = table.data('table').options;
+        if (options.resetCols || !options.headerDiv) {
+            if (options.headerDiv)
+                options.headerDiv.empty();
+            options.headerDiv = $('#' + options.id + ' .c-head', table);
+            var cols = options.columns;
+            var css = '<style type="text/css">';
+            var cls = '';
+            for (var ci = 0; ci < cols.length; ci++) {
+                var colstyle = '';
+                var col = cols[ci];
+                if (col.width)
+                    colstyle += 'width:' + col.width + 'px;';
+                if (col.align)
+                    colstyle += 'text-align:' + col.align + ';';
+                if (col.hidden)
+                    colstyle += 'display:none;';
+                if (colstyle.length > 0) {
+                    css += '.t-c' + col.field + '{' + colstyle + '}';
+                    cls += '<th class=".t-c' + col.field + '">' + col.title + '</th>';
+                    col.styleable = true;
+                } else {
+                    cls += '<th>' + col.title + '</th>';
+                }
+                if (col.formatter !== undefined && typeof col.formatter === 'string') {
+                    col.formatter = eval(col.formatter);
+                } else if (col.formatter === null)
+                    col.formatter = undefined;
+            }
+            css += '</style>';
+            options.headerDiv.append(css);
+            options.headerDiv.append(cls);
+        }
+        options.info.text('');
+        options.totalCount = (options.rows.length % options.pageSize) == 0 ? (options.rows.length / options.pageSize) : (parseInt(options.rows.length / options.pageSize) + 1);
+        bindingRows(options);
+    }
+
+    function bindingRows(options, prev, next) {
+        var rowStart = options.pageNumber * options.pageSize;
+        if (next && options.pageNumber < options.totalCount) {
+            rowStart += options.pageSize;
+            options.pageNumber += 1;
+        } else if (prev && options.pageNumber > 0) {
+            rowStart -= options.pageSize;
+            options.pageNumber -= 1;
+        }
+        var offset = (options.rows.length - rowStart);
+        if (offset <= 0) {
+            alert('已经为最后一页...');
+            return;
+        }
+        if (options.rowsDiv)
+            options.rowsDiv.empty();
+        options.info.text('当前为第' + (options.pageNumber + 1) + '页，共' + options.totalCount + '页');
+        var len = offset > options.pageSize ? options.pageSize : offset;
+        for (var ri = 0; ri < len; ri++) {
+            var row = options.rows[rowStart + ri];
+            var rl = '<tr id="r-' + (rowStart + ri) + '">';
+            for (var ci = 0; ci < options.columns.length; ci++) {
+                var col = options.columns[ci];
+                var val;
+                if (col.formatter)
+                    val = col.formatter(row[ci], row, rowStart + ri);
+                else
+                    val = row[ci];
+                var style = '';
+                if (col.styler)
+                    style += col.styler(row[ci], row, rowStart + ri);
+                rl += '<td';
+                if (col.styleable)
+                    rl += ' class="t-c' + col.field;
+                if (style)
+                    rl += ' style:' + style;
+                rl += '>' + val + '</td>';
+            }
+            rl += '</tr>';
+            var _row = $(rl).appendTo(options.rowsDiv);
+            if (options.onDblClickRow) {
+                $(_row).bind("dblclick", function (e) {
+                    options.onDblClickRow(e, row);
+                });
+            }
+            if (options.onRowContextMenu) {
+                $(_row).bind("contextmenu", function (e) {
+                    options.onRowContextMenu(e, row);
+                });
+            }
+            if (options.onCellContextMenu) {
+                $('td', _row).each(function (i, item) {
+                    $(item).bind("contextmenu", function (e) {
+                        options.onCellContextMenu(e, row, row[i], i);
+                    });
+                });
+            }
+        }
     }
 
     $.fn.table = function (options, param) {
@@ -48,8 +178,8 @@
                     options: $.extend({}, $.fn.table.defaults, options)
                 });
             }
-            _bindingColumns($(this));
-
+            bindingClass($(this), state.options);
+            bindingData($(this));
         });
     };
     $.fn.table.methods = {
@@ -57,15 +187,28 @@
             return $.data(jq[0], 'table').options;
         },
         set: function (jq, obj) {
-
+            $.extend($.data(jq[0], 'table').options, obj);
+            bindingData(jq[0]);
         },
         clear: function (jq) {
-
+            clear(jq[0]);
         }
     };
 
     $.fn.table.defaults = {
-        columns: [],
-        frozenable: true
+        columns: [],/*
+                      [{field:'',title:'',width:'',align:'',hidden:false,formater:function(val,row,rowindex){},styler:function(val,row,rowindex){}}]
+                    */
+        rows: [],
+        border: true,
+        resetCols: false,
+        onRowsContextMenu: function (e, row) { },
+        onCellContextMenu: function (e, row, cell, colindex) { },
+        onDblClickRow: function (row, index) { },
+        onContextMenu: function (e) { },
+        width: 'auto',
+        height: 'auto',
+        pageSize: 50,
+        pageNumber: 0
     };
 })(jQuery);
---
+++ WebApp/Scripts/test.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    var table = $('#container').table({
+        columns: [{ field: 'col1', title: '列1' }, { field: 'col2', title: '列2' }],
+        rows: [
+            ['nihao1', 'zhende1'], ['nihao2', 'zhende2'], ['nihao3', 'zhende3'], ['nihao4', 'zhende4'], ['nihao5', 'zhende5'],
+             ['nihao6', 'zhende6'], ['nihao7', 'zhende7'], ['nihao8', 'zhende8'], ['nihao9', 'zhende9'],
+            ['nihao10', 'zhende10'], ['nihao11', 'zhende11'], ['nihao12', 'zhende12'], ['nihao13', 'zhende13'], ['nihao14', 'zhende14'],
+            ['nihao15', 'zhende15'], ['nihao16', 'zhende16'], ['nihao17', 'zhende17'], ['nihao18', 'zhende18'],
+             ['nihao19', 'zhende19'], ['nihao20', 'zhende20'], ['nihao21', 'zhende21'], ['nihao22', 'zhende22'], ['nihao23', 'zhende23']
+        ],
+        //border: false,
+        width: '100%',//600,
+        height:100,
+        pageSize: 10,
+        onRowContextMenu: function (e, row) {
+
+        }
+    });
+});
